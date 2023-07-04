665dafc3bd0884d98540d3237b38d855365d58ac
hoilbo <125351011+hoilbo@users.noreply.github.com>
2023-07-04 22:34:20 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'OBNAH-a42a9dbcf98f45ec822b32fa44c4ddcc',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'OBNAH',
+  domain: 'https://www.notion.so/OBNAH-a42a9dbcf98f45ec822b32fa44c4ddcc',
+  author: 'HANBO',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
